35deef4cb04bf27a5f690eb5b193ba9ee9a6b514
muhammettylmz <muhammett.yilmazz@outlook.com>
2023-09-29 01:48:32 +0300

    elm327 datasheet eklendi. obd2 için driver klasör açıldı. standart pid test için bir kaç tane eklendi. başlagıç olarak

---
+++ m95_test1/Core/Driver/obd2/obd2.c
@@ -0,0 +1,10 @@
+/*
+ * obd2.c
+ *
+ *  Created on: Sep 29, 2023
+ *      Author: muham
+ */
+
+#include "obd2.h"
+
+
---
+++ m95_test1/Core/Driver/obd2/obd2.h
@@ -0,0 +1,16 @@
+/*
+ * obd2.h
+ *
+ *  Created on: Sep 29, 2023
+ *      Author: muham
+ */
+
+#ifndef DRIVER_OBD2_OBD2_H_
+#define DRIVER_OBD2_OBD2_H_
+
+#include "obd2pids.h"
+
+
+
+
+#endif /* DRIVER_OBD2_OBD2_H_ */
